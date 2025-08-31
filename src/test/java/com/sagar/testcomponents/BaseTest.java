434d4f63c838dd31141684fc5b16a6e697560486
vidhyaSagar23 <vidhyasagar2k01@gmail.com>
2025-08-31 23:45:26 +0530

    Modified Basetest

--- src/test/java/com/sagar/testcomponents/BaseTest.java
+++ src/test/java/com/sagar/testcomponents/BaseTest.java
@@ -4,10 +4,7 @@ import com.sagar.pageobjects.LandingPage;
 import com.sagar.utils.Utilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 import java.util.Properties;
@@ -33,14 +30,7 @@ public class BaseTest {
         applicationProperties = utilities.loadProperties("Application.properties");
         String browser = applicationProperties.getProperty("browser");
         if(browser.equals("chrome")){
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless");
-            options.addArguments("--headless=new");
-            options.addArguments("--window-size=1920,1080");
-            options.addArguments("--disable-gpu");
-            options.addArguments("--no-sandbox");
-            options.addArguments("--disable-dev-shm-usage");
-            driver = new ChromeDriver(options);
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10000));
         }
